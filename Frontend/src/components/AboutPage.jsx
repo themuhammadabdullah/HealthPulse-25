17c8bbf671e0ec83651d31b227a2f82399fc4348
Muhammad Abdullah <131662971+themuhammadabdullah@users.noreply.github.com>
2025-05-01 09:32:46 -0700

    .

--- Frontend/src/components/AboutPage.jsx
+++ Frontend/src/components/AboutPage.jsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
   const features = [
     {
       icon: <CircleSlash className="w-6 h-6 text-blue-800" />,
-      title: "Intelligentsss Analytics",
+      title: "Intelligent Analytics",
       description: "Advanced AI algorithms for precise healthcare predictions"
     },
     {
